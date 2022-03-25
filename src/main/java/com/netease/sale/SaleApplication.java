6a4166c323caad9e1232dae90867134bff8ff221
pweiz1 <305229084@qq.com>
2022-03-25 13:01:50 +0800

    内容销售系统

--- src/main/java/com/netease/sale/SaleApplication.java
+++ src/main/java/com/netease/sale/SaleApplication.java
@@ -1,7 +1,10 @@
 package com.netease.sale;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SaleApplication {
